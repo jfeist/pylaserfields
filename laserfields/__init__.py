89d3094a92a4e8c36e81984eb93de3ec1edc8db3
Johannes Feist <johannes.feist@gmail.com>
2023-02-12 00:12:09 +0100

    updates

--- laserfields/__init__.py
+++ laserfields/__init__.py
@@ -6,4 +6,5 @@ __all__ = ['make_laser_field', 'GaussianLaserField', 'SinExpLaserField',
            'LinearFlatTopLaserField', 'Linear2FlatTopLaserField', 'InterpolatingLaserField']
 
 from .laserfields import (make_laser_field, GaussianLaserField, SinExpLaserField,
-                          LinearFlatTopLaserField, Linear2FlatTopLaserField, InterpolatingLaserField)
+                          LinearFlatTopLaserField, Linear2FlatTopLaserField, InterpolatingLaserField,
+                          au_as, au_wcm2toel2, au_wcm2, au_m, au_cm, au_nm, au_c, au_eV, au_m_He, au_m_n)
